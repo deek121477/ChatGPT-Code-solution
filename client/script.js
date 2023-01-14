6f0c515c7130d44b4e8cabf15b65993574a7ce9c
Deeksha Jagtap <deekshajagtap01@gmail.com>
2023-01-14 17:56:18 +0530

    localhost 5000 added again

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('https://code-solution.onrender.com', {
+  const response = await fetch('https://localhost:5000', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
